b6a6f227d24ce44f5a64b92527cac5172a9c40b6
David Geary <davidgeary94@hotmail.co.uk>
2025-08-12 20:39:48 +0100

    Fixed graph component.

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,152 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import imgURL from "./assets/football.svg";
 import Graph from "./components/Graph";
+import * as d3 from "d3";
+import csvData from "./super-six-scores.csv";
+
+// Define lists of season participants
+const seasonOneNames = [
+  "andy",
+  "david",
+  "jake",
+  "james",
+  "jonnie",
+  "josh",
+  "sam",
+];
+const seasonTwoNames = [
+  "andy",
+  "david",
+  "ed",
+  "jack",
+  "jake",
+  "james",
+  "jonnie",
+  "josh",
+  "sam",
+];
+const seasonThreeNames = [
+  "andy",
+  "david",
+  "ed",
+  "jack",
+  "jake",
+  "james",
+  "jonnie",
+  "josh",
+  "sam",
+];
 
 function App() {
+  const [season, setSeason] = useState("24-25");
+  const [rawData, setRawData] = useState(null);
+  const [graphData, setGraphData] = useState(null);
+  const [players, setPlayers] = useState(seasonThreeNames);
+
+  useEffect(() => {
+    const getData = async (season) => {
+      console.log("Effect triggered.");
+      console.log(`season is: ${season}`);
+      const rawData = await d3.csv(csvData);
+      let seasonData = null;
+      switch (season) {
+        case "22-23":
+          console.log("Getting S1 data...");
+          seasonData = seasonOneNames.map(function (name) {
+            return {
+              name: name,
+              values: rawData
+                .filter((d) => d.name === name)
+                .map((d) => ({
+                  round: +d.round,
+                  score: +d.score,
+                  score_sum: +d.score_sum,
+                })),
+            };
+          });
+          break;
+        case "23-24":
+          console.log("Getting S2 data...");
+          seasonData = seasonTwoNames.map(function (name) {
+            return {
+              name: name,
+              values: rawData
+                .filter((d) => d.s2_name === name)
+                .map((d) => ({
+                  round: +d.s2_round,
+                  score: +d.s2_score,
+                  score_sum: +d.s2_score_sum,
+                })),
+            };
+          });
+          break;
+        case "24-25":
+          console.log("Getting S3 data...");
+          seasonData = seasonThreeNames.map(function (name) {
+            return {
+              name: name,
+              values: rawData
+                .filter((d) => d.s3_name === name)
+                .map((d) => ({
+                  round: +d.s3_round,
+                  score: +d.s3_score,
+                  score_sum: +d.s3_score_sum,
+                  correct_results: +d.s3_correct_results,
+                  correct_scores: +d.s3_correct_scores,
+                })),
+            };
+          });
+          break;
+        default:
+          console.log("No recognised season toggle!");
+      }
+      console.log("Setting state...");
+      setRawData(rawData);
+      setGraphData(seasonData);
+      setPlayers(
+        season === "22-23"
+          ? seasonOneNames
+          : season === "23-24"
+          ? seasonTwoNames
+          : seasonThreeNames
+      );
+    };
+    getData(season);
+  }, [season]);
+
   return (
     <>
       <div id="title">
-        <img id="football" src={imgURL} alt="Logo" width="60" height="60"/>
+        <img id="football" src={imgURL} alt="Logo" width="60" height="60" />
         <h1>Super 6 League Tracker</h1>
       </div>
       <h2>QUE SERA SERA, WE'RE GOING TO WORMBELLY</h2>
-      <Graph />
+      <div id="seasons">
+        <label htmlFor="season">Season:</label>
+        <select
+          name="season"
+          id="season-select"
+          value={season}
+          onChange={(e) => {
+            setSeason(e.target.value);
+            setGraphData(null);
+          }}
+        >
+          <option value="24-25">2024/2025</option>
+          <option value="23-24">2023/2024</option>
+          <option value="22-23">2022/2023</option>
+        </select>
+      </div>
+      {graphData !== null ? (
+        <Graph
+          season={season}
+          data={graphData}
+          rawData={rawData}
+          players={players}
+        />
+      ) : (
+        <p>Loading...</p>
+      )}
     </>
   );
 }
